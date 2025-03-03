9a7ab912cdc70330daf053d78c87663e7d9a3d78
WinGKuza <danilkuzn2004@gmail.com>
2025-03-03 18:40:12 +0700

    Созданы Model, View, ViewModel

---
+++ Model/DocumentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Compiler.Model
+{
+    internal class DocumentModel : BaseModel
+    {
+        private string _fileName;
+        private string _filePath;
+        private FlowDocument _document;
+
+        public string FileName
+        {
+            get => _fileName;
+            set => _fileName = value;
+        }
+        public DocumentModel() { }
+    }
+}
